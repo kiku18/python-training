552df5537d7342cd2570f7bd59f2e2089ba316c9
Kishore Kumar <kumar.kishore646@gmail.com>
2022-07-18 08:50:01 +0530

    Initial commit

---
+++ arithmeticoperator.py
@@ -0,0 +1,33 @@
+
+a=int(input("enter true number1"))
+b=int(input("enter true number2"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)     #floor division
+print(a%b)      #modules
+print(a**b)     #exponential
+'''
+a=int(input("enter true number1"))
+b=int(input("enter true number2"))
+print(f"the sum of {a} and {b} is = {a+b}")
+print(f"the sub of {a} and {b} is = {a-b}")
+print(f"the product of {a} and is {b} = {a*b}")
+print(f"the division of {a} and {b} is = {a/b}")
+'''''
+c=input("enter true number1")
+d=input("enter true number2")
+print(c+d)
+print(c*3)
+print(d*5)
+
+firstname=input("enter the firstname")
+lastname=input("enter the lastname")
+fullname= firstname + 'kns95' + lastname + '@gmail.com'
+print(fullname)
+
+firstname=input("enter the firstname")
+lastname=input("dont enter the last name stupid")
+fullname= firstname+lastname+"useless guy"
+print(fullname)
