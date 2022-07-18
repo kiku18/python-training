227c0cc37d8d4033cb9cf262f13f70955b85751f
Kishore Kumar <kumar.kishore646@gmail.com>
2022-07-18 08:30:32 +0530

    Initial commit

--- testworkout.py
+++ testworkout.py
@@ -1,4 +1,3 @@
-'''
 a=5
 b=6
 
@@ -34,7 +33,7 @@ c=[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]],[[13,14,15],[16,17,18]]
 c1=np.array(c)
 print(c)
 print(c1)
-'''
+
 import numpy as np
 a=np.arange(8)
 print(a)
