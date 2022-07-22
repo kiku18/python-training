6bdef1bb0f2c4e711f3a4b0eefc3dbaa08283ac7
Kishore Kumar <kumar.kishore646@gmail.com>
2022-07-22 19:29:30 +0530

    Initial commit.

--- arithmeticoperator.py
+++ arithmeticoperator.py
@@ -8,14 +8,14 @@ print(a/b)
 print(a//b)     #floor division
 print(a%b)      #modules
 print(a**b)     #exponential
-'''
+
 a=int(input("enter true number1"))
 b=int(input("enter true number2"))
 print(f"the sum of {a} and {b} is = {a+b}")
 print(f"the sub of {a} and {b} is = {a-b}")
 print(f"the product of {a} and is {b} = {a*b}")
 print(f"the division of {a} and {b} is = {a/b}")
-'''''
+
 c=input("enter true number1")
 d=input("enter true number2")
 print(c+d)
